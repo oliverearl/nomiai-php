8bafd9c4ecf70a53e548760c9040fab97dc7b99c
Oliver Earl <oliver@oliverearl.co.uk>
2024-11-27 14:27:13 +0000

    Fix messaging logic and add means to message Nomis directly.

--- src/Actions/ManagesChats.php
+++ src/Actions/ManagesChats.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Nomiai\PhpSdk\Actions;
 
 use Nomiai\PhpSdk\Resources\MessageSet;
+use Nomiai\PhpSdk\Resources\Nomi;
 
 /** @mixin \Nomiai\PhpSdk\Traits\MakesHttpRequests */
 trait ManagesChats
@@ -15,9 +16,20 @@ trait ManagesChats
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \JsonException
      */
+    public function sendMessageToNomi(Nomi $nomi, string $message): MessageSet
+    {
+        return $this->sendMessage($nomi->uuid, $message);
+    }
+
+    /**
+     * Send a message to a given ID.
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \JsonException
+     */
     public function sendMessage(string $id, string $message): MessageSet
     {
-        $response = $this->post("/v1/chats/{$id}", ['messageText' => $message]);
+        $response = $this->post("/v1/nomis/{$id}/chat", ['messageText' => $message]);
 
         return MessageSet::make($response);
     }
