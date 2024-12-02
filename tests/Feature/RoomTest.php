1906d83c19dcf581f3d7533c57a0f309b3ad4f5a
Oliver Earl <oliver@oliverearl.co.uk>
2024-12-02 13:44:16 +0000

    Add first two feature tests for rooms.

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -2,10 +2,52 @@
 
 declare(strict_types=1);
 
-describe('rooms', function (): void {
-    it('can get a list of rooms associated with an account', function (): void {})->todo();
+use Nomiai\PhpSdk\Constants\HttpMethod;
+use Nomiai\PhpSdk\Constants\HttpStatus;
+use Nomiai\PhpSdk\Resources\Room;
 
-    it('can get a specific room by id', function (): void {})->todo();
+describe('rooms', function (): void {
+    it('can get a list of rooms associated with an account', function (): void {
+        $room = $this->room();
+
+        $api = $this->dummy(
+            uri: '/v1/rooms',
+            method: HttpMethod::GET,
+            status: HttpStatus::OK,
+            body: [
+                'rooms' => [$room],
+            ],
+        );
+
+        $retrievedRooms = $api->getRooms();
+
+        expect($retrievedRooms)
+            ->toBeArray()
+            ->toContainOnlyInstancesOf(Room::class);
+
+        $retrievedRoom = reset($retrievedRooms);
+
+        expect($retrievedRoom)
+            ->toBeInstanceOf(Room::class)
+            ->toArray()->toEqual($room->toArray());
+    });
+
+    it('can get a specific room by id', function (): void {
+        $room = $this->room();
+
+        $api = $this->dummy(
+            uri: "/v1/rooms/{$room->uuid}",
+            method: HttpMethod::GET,
+            status: HttpStatus::OK,
+            body: $room->toArray(),
+        );
+
+        $retrievedRoom = $api->getRoom($room->uuid);
+
+        expect($retrievedRoom)
+            ->toBeInstanceOf(Room::class)
+            ->toArray()->toEqual($room->toArray());
+    });
 
     it('can create a room', function (): void {})->todo();
 
