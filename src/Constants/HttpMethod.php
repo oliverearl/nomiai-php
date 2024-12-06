82eb305c0567064d9a08e305c0307fec5389859a
Oliver Earl <oliver@oliverearl.co.uk>
2024-12-06 22:32:38 +0000

    Remove spacing

--- src/Constants/HttpMethod.php
+++ src/Constants/HttpMethod.php
@@ -7,12 +7,8 @@ namespace Nomiai\PhpSdk\Constants;
 abstract readonly class HttpMethod
 {
     public const string GET = 'GET';
-
     public const string POST = 'POST';
-
     public const string PUT = 'PUT';
-
     public const string PATCH = 'PATCH';
-
     public const string DELETE = 'DELETE';
 }
